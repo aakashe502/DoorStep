646ac6d3a0ac961cd7c6f49aa55c0edd5e81d032
Dheerendra choudhary <dheerendrachoudhary340@gmail.com>
2021-06-03 22:51:56 +0530

    Added radio button for shop category

--- app/src/main/java/com/hadIt/doorstep/fragment_ui/home/ViewShopProducts.java
+++ app/src/main/java/com/hadIt/doorstep/fragment_ui/home/ViewShopProducts.java
@@ -9,18 +9,26 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.hadIt.doorstep.R;
 import com.hadIt.doorstep.Repository.DataRepository;
 import com.hadIt.doorstep.cache.model.AdminProductModel;
+import com.hadIt.doorstep.utils.Constants;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -29,10 +37,11 @@ public class ViewShopProducts extends AppCompatActivity {
     RecyclerView recyclerView;
     public ArrayList<AdminProductModel> arrayList;
     public Button addprod;
-    FirebaseFirestore firestore;
+    public FirebaseFirestore firebaseFirestore;
     private DataRepository dataRespository;
-
+    private GridLayout gridLayout;
     public Toolbar toolbar;
+    private AdminAddapter modelAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +49,9 @@ public class ViewShopProducts extends AppCompatActivity {
         setContentView(R.layout.activity_view_category);
 
         dataRespository = new DataRepository(getApplication());
-        final String sessionId = getIntent().getStringExtra("grocery");
+        final String shopUid = getIntent().getStringExtra("grocery");
         final String shopName = getIntent().getStringExtra("shopName");
+        final String shopType = getIntent().getStringExtra("shopType");
 
         toolbar = findViewById(R.id.toolBar);
         toolbar.setTitle(shopName + " Dashboard");
@@ -57,29 +67,59 @@ public class ViewShopProducts extends AppCompatActivity {
             }
         });
 
+        gridLayout = findViewById(R.id.radioBtn);
+        RadioGroup radioGroup = new RadioGroup(this);
+        radioGroup.setOrientation(LinearLayout.VERTICAL);
+        getChipGroup(shopType, radioGroup);
+
         recyclerView = findViewById(R.id.productrecycler);
         arrayList = new ArrayList<>();
-        firestore = FirebaseFirestore.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        modelAdapter = new AdminAddapter(arrayList, this);
 
-        final AdminAddapter modelAdapter = new AdminAddapter(arrayList, this);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                arrayList.clear();
+                modelAdapter.notifyDataSetChanged();
+                RadioButton radioButton = group.findViewById(checkedId);
+                setRecyclerView(radioButton, shopType, shopUid);
+            }
+        });
+    }
 
+    private void setRecyclerView(RadioButton radioButton, String shopType, String shopUid) {
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(gridLayoutManager);
-        firestore.collection("Products").whereEqualTo("shopUid", sessionId)
-            .get()
-            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                    if (task.isSuccessful()) {
-                        for (DocumentSnapshot dpc : task.getResult().getDocuments()) {
-                            AdminProductModel productInfoModel = dpc.toObject(AdminProductModel.class);
-                            arrayList.add(productInfoModel);
+
+        firebaseFirestore.collection("Products").whereEqualTo("shopUid", shopUid).whereEqualTo("productCategory", shopType+" "+radioButton.getText())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot dpc : task.getResult().getDocuments()) {
+                                AdminProductModel productInfoModel = dpc.toObject(AdminProductModel.class);
+                                arrayList.add(productInfoModel);
+                            }
+                            modelAdapter.notifyDataSetChanged();
                         }
-                        modelAdapter.notifyDataSetChanged();
                     }
-                }
-            });
+                });
 
         recyclerView.setAdapter(modelAdapter);
+    }
+
+    private void getChipGroup(String shopType, RadioGroup radioGroup) {
+        String[] product = new Constants().products.get(shopType);
+        RadioGroup.LayoutParams layoutParams;
+        for(String i: product){
+            RadioButton radioButton = new RadioButton(this);
+            radioButton.setText(i);
+
+            layoutParams = new RadioGroup.LayoutParams(RadioGroup.LayoutParams.WRAP_CONTENT, RadioGroup.LayoutParams.WRAP_CONTENT);
+            radioGroup.addView(radioButton, layoutParams);
+        }
+        gridLayout.addView(radioGroup);
     }
 }
--- app/src/main/java/com/hadIt/doorstep/utils/Constants.java
+++ app/src/main/java/com/hadIt/doorstep/utils/Constants.java
@@ -1,6 +1,83 @@
 package com.hadIt.doorstep.utils;
 
+import java.util.HashMap;
+
 public class Constants {
+    public Constants() {
+        products.put("VEGETABLES & FRUITS", new String[]{"Vegetables",
+                "Fruits",
+                "Others"});
+
+        products.put("GROCERY", new String[]{"Bakery and Bread",
+                "Rice and Pulses",
+                "Oils, Sauces, Salad Dressings, and Condiments",
+                "Cereals and Breakfast Foods",
+                "Soups and Canned Goods",
+                "Frozen Foods",
+                "Dairy, Cheese, and Eggs",
+                "Others"});
+
+        products.put("BEVERAGES", new String[]{"Drinking water",
+                "Cold Drinks",
+                "Flavoured Cola",
+                "Others"});
+
+        products.put("NON_VEG", new String[]{"Meat",
+                "Chicken",
+                "Fish",
+                "Others"});
+
+        products.put("CAKES & BAKERY", new String[]{"Cake",
+                "Chocolates",
+                "Pastries",
+                "Muffins",
+                "Bread and Buns",
+                "Ice Cream Raw materials",
+                "Bakery",
+                "Others"});
+
+        products.put("BOOKS & STATIONERY", new String[]{"Notebook",
+                "Pen & Pencil",
+                "Eraser and Sharpener",
+                "Stickers and NameChits",
+                "Boxes",
+                "Covers and Envelopes",
+                "Gift and Chart Paper",
+                "Tape and Stapler",
+                "Comics",
+                "Others"});
+
+        products.put("NUTRITION & HEALTHCARE", new String[]{"Tablets",
+                "Syrups",
+                "Injection",
+                "Protein and Powder",
+                "Sanitary Pads",
+                "Others"});
+
+        products.put("BEAUTY AND PERSONAL CARE", new String[]{"Baby Products",
+                "Lotions oil power and cream",
+                "Spray and Fragrance",
+                "Hair",
+                "Makeup",
+                "Oral Hygiene",
+                "Shaving Preparations",
+                "Skin Care",
+                "Manicure",
+                "Pedicure",
+                "Others"});
+
+        products.put("HOME-MADE", new String[]{"Pickle",
+                "Spices",
+                "Others"});
+
+        products.put("DAIRY PRODUCTS", new String[]{"Milk",
+                "Paneer",
+                "Curd",
+                "Others"});
+    }
+
+    public static HashMap<String, String[]> products = new HashMap<>();
+
     public static final String FCM_KEY = "AAAA3Sz0p7I:APA91bHbnd7aabTOpzjTRPGS7h3K-1pyw0scF1F67Xzui49UDMPwOc_SQgygSHEGQ3hgWWe-3d4sEhNQ5X3vhH5f3eKU7zRTEFSp3JjJZiMNTHKUQkD3jXZzSoIDDsCtdoOptiNVH77N";
     public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
 
@@ -8,13 +85,11 @@ public class Constants {
             "VEGETABLES & FRUITS",
             "GROCERY", // General stores
             "BEVERAGES",
-            "NON-VEG",
-            "CAKES & MORE",
-            "HARVEST",
-            "BREAD",
-            "CLEANING",
+            "NON_VEG",
+            "CAKES & BAKERY",
             "BOOKS & STATIONERY",
             "NUTRITION & HEALTHCARE",
+            "BEAUTY AND PERSONAL CARE",
             "HOME-MADE", // Khada mashala, achar
             "DAIRY PRODUCTS"
     };
