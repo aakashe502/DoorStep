d7469f0ab683d91303e1410dbb548cf6658ca819
Axil-Blaze <aakashe502@users.noreply.github.com>
2021-06-22 22:23:47 +0530

    Added batch for cart and notification

--- app/src/main/java/com/hadIt/doorstep/homePage/HomePage.java
+++ app/src/main/java/com/hadIt/doorstep/homePage/HomePage.java
@@ -2,6 +2,8 @@ package com.hadIt.doorstep.homePage;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
@@ -160,5 +162,22 @@ public class HomePage extends AppCompatActivity {
         else{
             bottomNavigationView.setSelectedItemId(R.id.navigation_home);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.icon_cart_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home1:
+            case R.id.notif1:
+            case R.id.dash1:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
